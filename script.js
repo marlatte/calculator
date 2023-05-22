eaf031c559d9d57f283797dd0b3f5741d7206b50
MarLatte <86508134+marlatte@users.noreply.github.com>
2023-05-22 14:24:36 -0400

    Connect buttons to basic JS

--- script.js
+++ script.js
@@ -1,3 +1,67 @@
+let onScreen = document.querySelector("#screen");
+
+const clearScreenBtn = document.querySelector("#clear");
+const invertBtn = document.querySelector("#invert");
+const percentBtn = document.querySelector("#percent");
+const divideBtn = document.querySelector("#divide");
+
+const buttons = document.querySelectorAll(".button");
+buttons.forEach(button => {
+	button.addEventListener("click", () => fireButton(button));
+});
+
+function fireButton(button) {
+	console.log(button.id);
+	switch (button.id) {
+		case "clear":
+			// IF onScreen ends in " 0"
+			allClear();
+			/*
+			ELSE
+				halfClear();
+					Replace final digits with "0"
+			*/
+			break;
+
+		case "invert":
+			console.log("I'm gonna invert someday!");
+			// lastNumber *= -1;
+			break;
+
+		case "percent":
+			console.log("I'm gonna make a percent someday!");
+			break;
+
+		case "equals":
+			onScreen.textContent = operate(onScreen.textContent);
+			break;
+	
+		default:
+			if (onScreen.textContent === "0") onScreen.textContent = "";
+			updateScreen(button.textContent)
+			break;
+	};
+}
+
+function updateScreen(value) {
+	switch (value) {
+		case "÷":
+		case "×":
+		case "–":
+		case "+":
+			onScreen.textContent += " " + value + " "
+			break;
+	
+		default:
+			onScreen.textContent += value
+			break;
+	}
+}
+
+function allClear() {
+	onScreen.textContent = "0";
+}
+
 function operate(str) {
 	const split = str.split(' '),
 		a = +split[0],
@@ -6,9 +70,9 @@ function operate(str) {
 
 	const methods = {
 		"+": (a, b) => a + b,
-		"-": (a, b) => a - b,
-		"*": (a, b) => a * b,
-		"/": (a, b) => a / b,
+		"–": (a, b) => a - b,
+		"×": (a, b) => a * b,
+		"÷": (a, b) => a / b,
 	};
 
 	if (!methods[opr] || isNaN(a) || isNaN(b)) {
@@ -18,17 +82,17 @@ function operate(str) {
 	return +(answer + Number.EPSILON).toFixed(5);
 }
 
-function testCalculations() {
-	for (let index = 0; index < 10; index++) {
-		let randA = Math.floor(Math.random() * 100) + 1;
-		let randB = Math.floor(Math.random() * 100) + 1;
-		let operators = {
-			symbol : ["+", "-", "*", "/"],
-			word : ["sum of", "difference between", "product of", "quotient of"],
-		};
-		let chooser = Math.floor(Math.random() * 4);
+// function testCalculations() {
+// 	for (let index = 0; index < 10; index++) {
+// 		let randA = Math.floor(Math.random() * 100) + 1;
+// 		let randB = Math.floor(Math.random() * 100) + 1;
+// 		let operators = {
+// 			symbol : ["+", "-", "×", "÷"],
+// 			word : ["sum of", "difference between", "product of", "quotient of"],
+// 		};
+// 		let chooser = Math.floor(Math.random() * 4);
 	
-		console.log(`The ${operators.word[chooser]} ${randA} and ${randB} is ${operate(`${randA} ${operators.symbol[chooser]} ${randB}`)}`);
-	};
-}
+// 		console.log(`The ${operators.word[chooser]} ${randA} and ${randB} is ${operate(`${randA} ${operators.symbol[chooser]} ${randB}`)}`);
+// 	};
+// }
 // testCalculations();
